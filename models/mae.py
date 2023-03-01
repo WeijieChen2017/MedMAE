18db189f4137ed39b957f8eb7b999e1e5498593e
WeijieChen2017 <weijiechen1994@gmail.com>
2023-02-28 20:09:12 -0600

    Good morning!

--- models/mae.py
+++ models/mae.py
@@ -285,7 +285,8 @@ def mae_vit_base_patch16_dec512d8b(**kwargs):
 
 def mae_vit_large_patch16_dec512d8b(**kwargs):
     model = MaskedAutoencoderViT(
-        modality_club=["MR", "PET"],
+        modality_club=["MR_brain", "PET_wb", "CT_brain", "CT_wb"],
+        img_size=256,
         patch_size=16, embed_dim=1024, depth=24, num_heads=16,
         decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
         mlp_ratio=4, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
---
+++ pre_processing_NAC_CT.py
@@ -0,0 +1,34 @@
+import nibabel as nib
+import numpy as np
+import os
+import glob
+
+NAC_list = sorted(glob.glob("./data/PET_256_MAX_MIN_2_Not_Normalized/*.nii.gz"))
+CT_list = sorted(glob.glob("./data/CT_256_MAX_MIN_2_Not_Normalized/*.nii.gz"))
+
+for file_path in NAC_list:
+    file_name = os.path.basename(file_path)
+    print(file_name, end=" -> ")
+    NAC_file = nib.load(file_path)
+    NAC_data = NAC_file.get_fdata()
+    print(NAC_data.shape)
+    NAC_data = np.resize(NAC_data, (256, 256, NAC_data.shape[2]))
+    NAC_data = NAC_data / 4000
+    new_NAC_file = nib.Nifti1Image(NAC_data, NAC_file.affine, NAC_file.header)
+    new_save_name = "./data/NAC_wb_norm/" + file_name
+    nib.save(new_NAC_file, new_save_name)
+
+for file_path in CT_list:
+    file_name = os.path.basename(file_path)
+    print(file_name, end=" -> ")
+    CT_file = nib.load(file_path)
+    CT_data = CT_file.get_fdata()
+    print(CT_data.shape)
+    CT_data = np.resize(CT_data, (256, 256, CT_data.shape[2]))
+    CT_data = (CT_data + 1000) / 4000
+    new_CT_file = nib.Nifti1Image(CT_data, CT_file.affine, CT_file.header)
+    new_save_name = "./data/CT_wb_norm/" + file_name
+    nib.save(new_CT_file, new_save_name)
+
+    
+
