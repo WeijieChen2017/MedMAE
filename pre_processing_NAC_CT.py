09dcc3944ff58ef7d4695b98551dc5ee2e5635a2
WeijieChen2017 <weijiechen1994@gmail.com>
2023-03-01 20:45:00 -0600

    Good morning!

--- pre_processing_NAC_CT.py
+++ pre_processing_NAC_CT.py
@@ -3,8 +3,8 @@ import numpy as np
 import os
 import glob
 
-NAC_list = sorted(glob.glob("./data/Iman/PET_256_MAX_MIN_2_Not_Normalized/*.nii.gz"))
-CT_list = sorted(glob.glob("./data/Iman/CT_256_MAX_MIN_2_Not_Normalized/*.nii.gz"))
+NAC_list = sorted(glob.glob("./data/Iman/PET_256_MAX_MIN_2_Not_Normalized/*.nii"))
+CT_list = sorted(glob.glob("./data/Iman/CT_256_MAX_MIN_2_Not_Normalized/*.nii"))
 
 for folder_path in ["./data/NAC_wb_norm/", "./data/CT_wb_norm/"]:
     if not os.path.exists(folder_path):
@@ -19,7 +19,7 @@ for file_path in NAC_list:
     NAC_data = np.resize(NAC_data, (256, 256, NAC_data.shape[2]))
     NAC_data = NAC_data / 4000
     new_NAC_file = nib.Nifti1Image(NAC_data, NAC_file.affine, NAC_file.header)
-    new_save_name = "./data/NAC_wb_norm/" + file_name
+    new_save_name = "./data/NAC_wb_norm/" + file_name + ".gz"
     nib.save(new_NAC_file, new_save_name)
 
 for file_path in CT_list:
@@ -31,7 +31,7 @@ for file_path in CT_list:
     CT_data = np.resize(CT_data, (256, 256, CT_data.shape[2]))
     CT_data = (CT_data + 1000) / 4000
     new_CT_file = nib.Nifti1Image(CT_data, CT_file.affine, CT_file.header)
-    new_save_name = "./data/CT_wb_norm/" + file_name
+    new_save_name = "./data/CT_wb_norm/" + file_name + ".gz"
     nib.save(new_CT_file, new_save_name)
 
     
