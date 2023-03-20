5de3e8d6c93ce9a08252d3f6e5d5a21c576f6a76
WeijieChen2017 <weijiechen1994@gmail.com>
2023-03-20 16:31:23 -0500

    Good morning!

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,9 @@ from .mae import MaskedAutoencoderViT
 from .mae import mae_vit_base_patch16
 from .mae import mae_vit_large_patch16
 from .mae import mae_vit_huge_patch14
+
+from .mae_cont import MaskedAutoencoderViT_cont
+from .mae_cont import mae_vit_base_patch16_cont
+from .mae_cont import mae_vit_large_patch16_cont
+from .mae_cont import mae_vit_huge_patch14_cont
+
